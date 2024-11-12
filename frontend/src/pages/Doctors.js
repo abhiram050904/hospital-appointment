cec36bc9cff784d559b86577ef1d33e13940e2fc
abhiram050904 <abhiram050904@gmail.com>
2024-11-12 11:02:02 +0530

    backend-1

--- frontend/src/pages/Doctors.js
+++ frontend/src/pages/Doctors.js
@@ -6,6 +6,8 @@ const Doctors = () => {
   const { speciality } = useParams();
   const { doctors } = useContext(Appcontext);
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter,setShowFliter]=useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,8 +19,9 @@ const Doctors = () => {
   return (
     <div>
       <p className='text-gray-600'>Browse through specialised doctors</p>
-      <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className=' flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`px-3 py-1 border rounded text-sm tranition-all sm:hidden ${showFilter? 'bg-primary text-white':''}`} onClick={()=>setShowFliter(prev=>!prev)}>Filters</button>
+        <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex' }`}>
           <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>GENERAL PHYSICIAN</p>
           <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>GYNECOLOGIST</p>
           <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>DERMATOLOGIST</p>
