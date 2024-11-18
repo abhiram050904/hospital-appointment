e82c7ada2a9de962aca0bb25171b100f7dd2269f
abhiram050904 <142649536+abhiram050904@users.noreply.github.com>
2024-11-18 09:23:53 +0530

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,7 +1,7 @@
 import doctorModel from "../models/doctorModel.js";
 import bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
-import appointmentModel from '../models/appointmentModel.js';
+import appointmentModel from '../models/AppointmentModel.js';
 
 const changeAvailability = async (req, res) => {
   try {
